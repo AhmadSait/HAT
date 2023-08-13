9fa69da9569b8393b4cc94be3ec02aed96fc45c1
AhmadSait <84393120+AhmadSait@users.noreply.github.com>
2023-08-13 15:21:08 +0100

    Update create-conda-env.sh

--- bin/create-conda-env.sh
+++ bin/create-conda-env.sh
@@ -1,13 +1,13 @@
 #!/bin/bash --login
-#SBATCH --time = 2:00:00
-#SBATCH --cpus-per-task = 2
-#SBATCH --mem = 8G
-#SBATCH --partition = debug
-#SBATCH --constraint = intel
-#SBATCH --job-name = create-conda-env
-#SBATCH --mail-type=ALL
-#SBATCH --output=bin/%x-%j-slurm.out
-#SBATCH --error=bin/%x-%j-slurm.err
+SBATCH --time = 2:00:00
+SBATCH --cpus-per-task = 2
+SBATCH --mem = 8G
+SBATCH --partition = debug
+SBATCH --constraint = intel
+SBATCH --job-name = create-conda-env
+SBATCH --mail-type=ALL
+SBATCH --output=bin/%x-%j-slurm.out
+SBATCH --error=bin/%x-%j-slurm.err
 # create the conda environment
 export ENV_PREFIX=$PWD/env
 conda env create --prefix $ENV_PREFIX --file environment.yml --force
